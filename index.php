be63ca41548c965022649d2524f13107f480bf99
unknown <juliana.praciano@041M2131081572.SLAB.UCB.BR>
2015-09-04 16:09:28 -0300

    Fixed 1

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 
 /**
  * Define the application's minimum supported PHP version as a constant so it can be referenced within the application.
+ echo'My JoomlaDay Brasil 2';
  */
 define('JOOMLA_MINIMUM_PHP', '5.3.10');
 
